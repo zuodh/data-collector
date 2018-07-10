d86792e12b114c607428ce642dd8b075ae5970e6
zuodh <zdh_china@163.com>
2018-07-10 14:10:58 +0800

    Update RemoteHttpConstants.java

--- src/main/java/com/data/shuzi/datacollector/common/RemoteHttpConstants.java
+++ src/main/java/com/data/shuzi/datacollector/common/RemoteHttpConstants.java
@@ -8,16 +8,16 @@ package com.data.shuzi.datacollector.common;
  * @Version 1.0
  **/
 public interface RemoteHttpConstants {
-    public final static String TOKNE_URL="http://47.96.185.5:8800";
-    public final static String PROJECT_URL="http://47.96.185.5:8900";
+    public final static String TOKNE_URL="";
+    public final static String PROJECT_URL="";
 
-    /**"tenantEname": "shuangliang",*/
-   public final static String TENANTENAME="shuangliang";
-    /**"name": "aliyun",*/
-    public final static String NAME="aliyun";
+    /**"tenantEname": "",*/
+   public final static String TENANTENAME="";
+    /**"name": "",*/
+    public final static String NAME="";
     /**"hash": "一个任意字符串"必须*/
-    public final static String PASSWORD="aliyun01";
+    public final static String PASSWORD="";
     /** "hash": "一个任意字符串"必须*/
-    public final static String HASH="123456";
+    public final static String HASH="";
 
 }
